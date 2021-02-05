80928db6ea0cb6a114291afbccb8770c2741e1b4
konanok <konanok@outlook.com>
2021-02-05 08:03:22 +0800

    重新构建： 发布文章：算法：Union-Find

--- manager.kts
+++ manager.kts
@@ -1,7 +1,6 @@
 import java.io.*
 import java.time.*
 import java.time.format.DateTimeFormatter
-import kotlin.system.exitProcess
 
 /**
  * Hugo博客管理工具
@@ -9,56 +8,20 @@ import kotlin.system.exitProcess
  * @author konanok<konanok@outlook.com>
  */
 
-val name = "manager"   // 名称
-val version = "v1.0.0" // 版本号
-val synopsis =         // 帮助文本
-"""
-Usage: $name <options> <command> <arguments>
+val rootPath = System.getProperty("user.dir")!!  // 博客根路径，默认取脚本执行路径
+val postsPath = "$rootPath/content/posts"        // 文章路径
+val imagesPath = "$rootPath/images"              // 图片路径
+val websitePath = "$rootPath/public"             // 博客网站路径
 
-Options:
-    -version, -v                        输出版本信息
-    -help, -h                           输出帮助信息
-    -quiet, -q                          静默执行命令
-
-Command:
-    createPost <title>                  创建文章，<title>：文章标题
-    saveImage                           上传图片，获取截图文件并上传至Github
-    deploy <message>                    部署博客，同时提交所有的子项目，<message>：Git提交的信息
-"""
+val draftsPath = "$postsPath/_draft"             // 草稿文件路径
+val capturesPath = "$imagesPath/_capture"        // 截图文件路径
 
 
-val rootPath = System.getProperty("user.dir")!! // 博客根路径
-val postsPath = "$rootPath/content/posts"       // 文章路径
-val imagesPath = "$rootPath/images"             // 图片路径
-val websitePath = "$rootPath/public"            // 博客网站路径
-
-val capturedImagesPath = "$imagesPath/capture"  // 截图存放文件路径
-val capturedImageFinder: File.() -> String = {  // 查找截图文件的方法，选取截图目录下的第一个文件
-    list()?.first()!!
-}
-
-
-var isQuiet = false                             // 是否静默执行命令
-
-if (args.isEmpty()) {
-    printHelp()
-    exitProcess(0)
-}
-
+// 参数解析
 when (args[0]) {
-    "-version", "-v" -> printVersion()
-    "-help", "-h" -> printHelp()
-    "createPost" -> createPost(args[1])
-    "saveImage" -> saveImage()
-    "deploy" -> deploy(args[1])
-    "-quiet", "-q" -> {
-        isQuiet = true
-        when (args[1]) {
-            "createPost" -> createPost(args[2])
-            "saveImage" -> saveImage()
-            "deploy" -> deploy(args[2])
-        }
-    }
+    "createPost", "new" -> createPost(args[1])
+    "saveImage", "get" -> saveImage()
+    "publish", "pub" -> publish(args.copyOfRange(1, args.size))
 }
 
 
@@ -75,31 +38,29 @@ fun createPost(title: String) {
         ---
         title: "$title"
         date: ${zonedDateTime.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)}
+        draft: true
         slug: p$timestamp
-        isCJKLanguage: true
-        showtoc: true
-        tocopen: true
         ---
     """.trimIndent()
 
-    File("$postsPath/${zonedDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE)}-$title.md").run {
+    File("$draftsPath/${zonedDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE)}-$title.md").run {
         createNewFile()
         writeText(frontMatter)
-        printInfo("create post: $absolutePath")
+        println("create post: $absolutePath")
     }
 }
 
 /**
- * 上传图片
+ * 上传图片，并获取图片路径
  */
 fun saveImage() {
-    val capturedImageName = File(capturedImagesPath).run(capturedImageFinder)
+    val captureName = File(capturesPath).list()?.first()!!
     val imageName = "i${System.currentTimeMillis()}.png"
 
     // images
     sh {
         // 移动截图文件至images仓库，并对其重命名
-        + "mv $capturedImagesPath/$capturedImageName $imagesPath/$imageName"
+        + "mv $capturesPath/$captureName $imagesPath/$imageName"
         // 提交至Github
         + "cd $imagesPath"
         + "git add ."
@@ -111,16 +72,16 @@ fun saveImage() {
 }
 
 /**
- * 部署博客
- * 
- * @param message Git提交的信息
+ * 发布博客网站
+ *
+ * @param message 构建信息
  */
-fun deploy(message: String) {
+fun publish(message: Array<String>) {
     // posts
     sh {
         + "cd $postsPath"
         + "git add ."
-        + "git commit -m \"$message\" |& cat"
+        + "git commit -m '${formatCommit(message)}' |& cat"
         + "git push"
     }
     // website
@@ -133,78 +94,38 @@ fun deploy(message: String) {
         // 提交至Github
         + "cd $websitePath"
         + "git add ."
-        + "git commit -m \"$message；重新构建\" |& cat"
+        + "git commit -m \$'重新构建：\n${formatCommit(message)}' |& cat"
         + "git push"
     }
     // blog
     sh {
         + "cd $rootPath"
         + "git add ."
-        + "git commit -m \"$message；重新构建\" |& cat"
+        + "git commit -m \$'重新构建：\n${formatCommit(message)}' |& cat"
         + "git push"
     }
 }
 
 
 /**
- * 输出帮助信息
- *
- * @param error 错误信息
+ * 格式化commit信息
  */
-fun printHelp(error: String = "") {
-    if (error.isNotBlank()) printError(error)
-    printInfo(synopsis)
-}
-
-/**
- * 输出版本信息
- */
-fun printVersion() {
-    printInfo("$name $version")
-}
-
-/**
- * 输出info日志
- */
-fun printInfo(info: String) {
-    println(info)
-}
-
-/**
- * 输出error日志
- */
-fun printError(error: String) {
-    System.err.println(error)
-}
-
-
-/**
- * 标准输出流监听器
- */
-class StandardOutListener(
-    inputStream: InputStream
-): Runnable {
-
-    private val reader = BufferedReader(InputStreamReader(inputStream))
-
-    private var isRunning = true
-
-    override fun run() {
-        while (isRunning) {
-            var line: String?
-            if (reader.readLine().also { line = it } != null) {
-                printInfo(line!!)
-            } else {
-                isRunning = false
+fun formatCommit(message: Array<String>): String {
+    return if (message.size == 1) {
+        if (message[0].contains("；")) {
+            formatCommit(message[0].split("；").toTypedArray())
+        } else {
+            message[0]
+        }
+    } else {
+        buildString {
+            message.forEachIndexed { i, s ->
+                append("$i. $s；\n")
             }
         }
-        reader.close()
-    }
-
-    fun shutdown() {
-        isRunning = false
     }
 }
+
 
 /**
  * 虚拟Shell
@@ -212,17 +133,23 @@ class StandardOutListener(
  * 用于在Shell环境下顺序执行一个或多个命令
  */
 class VirtualShell(
-    // 是否静默执行（忽略输出）
-    private val isQuiet: Boolean = false
+    /**
+     * 是否为debug模式，true：输出命令执行的详细日志，默认为false
+     */
+    private val isDebug: Boolean = false,
+    /**
+     * 系统真实执行命令的shell，默认为zsh
+     */
+    actualShell: String = "/bin/zsh"
 ) {
 
     // 系统shell进程
-    private val shellProcess = Runtime.getRuntime().exec("/bin/zsh")
+    private val shellProcess = Runtime.getRuntime().exec(actualShell)
 
     private val commandWriter = BufferedWriter(OutputStreamWriter(shellProcess.outputStream))
 
-    private val stdoutListener = if (isQuiet) null else StandardOutListener(shellProcess.inputStream)
-    private val stderrListener = if (isQuiet) null else StandardOutListener(shellProcess.errorStream)
+    private val stdoutListener = if (isDebug) StandardOutListener(shellProcess.inputStream) else null
+    private val stderrListener = if (isDebug) StandardOutListener(shellProcess.errorStream) else null
 
     // 需要执行的命令
     private val commands = arrayListOf<String>()
@@ -232,11 +159,12 @@ class VirtualShell(
         commands.add(this)
     }
 
+
     /**
      * 顺序执行所有输入的命令，只能调用一次
      */
     fun execute() {
-        if (!isQuiet) startListening()
+        if (isDebug) startListening()
 
         for (cmd in commands) execute(cmd)
         execute("exit") // 最后执行exit命令来使shell正常退出，否则process会一直阻塞
@@ -247,7 +175,7 @@ class VirtualShell(
     }
 
     private fun execute(cmd: String) {
-        if (cmd != "exit") printInfo("➜ $cmd")
+        if (cmd != "exit") println("➜ $cmd")
         commandWriter.write(cmd)
         commandWriter.newLine()
         commandWriter.flush()
@@ -262,11 +190,43 @@ class VirtualShell(
         stdoutListener?.shutdown()
         stderrListener?.shutdown()
     }
+
+
+    /**
+     * 标准输出流监听器
+     */
+    private class StandardOutListener(
+        inputStream: InputStream
+    ): Runnable {
+
+        private val reader = BufferedReader(InputStreamReader(inputStream))
+
+        private var isRunning = true
+
+        override fun run() {
+            while (isRunning) {
+                var line: String?
+                if (reader.readLine().also { line = it } != null) {
+                    println(line!!)
+                } else {
+                    isRunning = false
+                }
+            }
+            reader.close()
+        }
+
+        fun shutdown() {
+            isRunning = false
+        }
+
+    }
+
 }
+
 
 /**
  * 在Shell环境下执行命令
  */
 fun sh(init: VirtualShell.() -> Unit) {
-    VirtualShell(isQuiet).apply(init).execute()
+    VirtualShell().apply(init).execute()
 }
